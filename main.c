cc52e94b4a47c64c96c681a2e8d7c71cc5ca7ebc
Jhon Eduar García Ortiz <jhon.garciao@udea.edu.co>
2023-11-02 17:48:04 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdint.h>
+#include<stdlib.h>
+#include "pico/stdlib.h"
+
+int main(int argc, char *argv[ ]) {
+    stdio_init_all();
+
+    while (1){
+        
+    }
+    
+}
